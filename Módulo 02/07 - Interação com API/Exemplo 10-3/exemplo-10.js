e50534942ec30e938b7722085994323b94439461
AlvesOne <rodrigo_pirateiro@hotmail.com>
2024-03-15 11:28:58 -0300

    update

--- Módulo 02/07 - Interação com API/Exemplo 10-3/exemplo-10.js
+++ Módulo 02/07 - Interação com API/Exemplo 10-3/exemplo-10.js
@@ -33,3 +33,27 @@ function cliqueCard(){
     let elementoID = this.getAttribute("data-id")
     window.location.href ="detalhes.html?produtoid="+ elementoID
 }
+
+let slideIndex = 0;
+function showSlide(index){
+    const slides = document.querySelectorAll('.slide');
+    if(index >= slides.length){
+        slideIndex = 0;
+    } else if(index < 0){
+        slideIndex = slides.length -1;
+    }
+    slides.forEach((slide) => {
+        slide.style.display = 'none';
+    });
+    slides[slideIndex].style.display = 'block';
+
+}
+function nextSlid(){
+    slideIndex++;
+    showSlide(slideIndex);
+}
+function prevSlid(){
+    slideIndex--;
+    showSlide(slideIndex);
+}
+showSlide(slideIndex)
