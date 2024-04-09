51784fcd3b8a3a44514fe60e577431146b491bb8
AlvesOne <rodrigo_pirateiro@hotmail.com>
2024-04-09 10:10:22 -0300

    update

--- Módulo 02/07 - Interação com API/Exemplo 10-3/detalhes.js
+++ Módulo 02/07 - Interação com API/Exemplo 10-3/detalhes.js
@@ -24,6 +24,7 @@ async function verDetalhe(){
             <div class="grupoValores">
                 <span class="valorCom">R$ ${(produtos[inProduto].valorComDesconto).toFixed(2).replace(".",",")}</span>
                 <span class="valorSem">R$ ${(produtos[inProduto].valorSemDesconto).toFixed(2).replace(".",",")}</span>
+                <p></p>
             </div>
             <a href="carrinho.html"><button class="buttonCard">Adicionar ao carrinho</button></a>
 
