d2a21729a22778761c93c7d81afe946cdb30eaea
AlvesOne <rodrigo_pirateiro@hotmail.com>
2024-07-24 11:32:34 -0300

    update

--- Modulo 03/09 - Codificação para Back-End/SiteFinal/apps/core/models.py
+++ Modulo 03/09 - Codificação para Back-End/SiteFinal/apps/core/models.py
@@ -38,3 +38,11 @@ class OrdemServico(models.Model):
     def __str__(self):
         return "OS: " + self.id + " | " + self.cliente.nome
     
+class Produto(models.Model):
+    nome_produto = models.CharField(max_length=100)
+    valor_produto = models.DecimalField(decimal_places=2, max_digits=10)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.nome_produto
+    
